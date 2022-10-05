11ff6c8fa24d86c7af78b1e0db70fbfd8c244753
suecmorin <suecmorin@gmail.com>
2022-10-04 19:46:16 -0700

    final push

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const api = require('./routes/index.js');
 const { v4: uuidv4 } = require ("uuid");
 
 const PORT = process.env.PORT || 3001;
@@ -15,7 +14,7 @@ app.use(express.static('public'));
 
 //get route for reading notes database
 app.get ('/api/notes', (req, res) => {
-fs.readFile(path_join(_dirname,'./db/db.json' ), "utf8", (error, notes) => {
+fs.readFile(path.join(__dirname,'./db/db.json' ), "utf8", (error, notes) => {
   if (error) {
     console.log("error reading note from db")
   }
@@ -24,32 +23,38 @@ fs.readFile(path_join(_dirname,'./db/db.json' ), "utf8", (error, notes) => {
 });
 
 //get route for writing to notes database
-app.post('./api/notes', (req, res) => {
-  const notes = JSON.parse(fs.readFile('./db/db.json'));
+app.post('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json',"utf8",  (error, notesarray) => {
+    if (error) {
+      console.log("error reading note from db")
+    }
+    let notes = JSON.parse(notesarray);
+ 
   const uniqueId = uuidv4();
 
   const newNotes = { id: uniqueId,
-    noteTitle: req.body.noteTitle,
-    noteText: req.body.noteText };
+    title: req.body.title,
+    text: req.body.text };
  
   notes.push(newNotes);
-  fs.writeFileSync('./db/db.json', JSON.stringify(notes), (err) => {
+  fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
    if (err) {
     console.log("Error writing note to db");
    }
-   res.json(notes);
-   console.log("New note added to db");
+   return res.json(notes);
+   //console.log("New note added to db");
   })
-});
-
-// GET Route for homepage
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+}) ;
 });
 
 // GET Route for notes page
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+// GET Route for homepage must come last
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
 app.listen(PORT, () => {
